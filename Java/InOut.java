9e693325e264d207503828367e360cf4f4da19f4
Anshul Prajapati <parjapatianshul695@gmail.com>
2025-07-09 12:00:36 +0530

    Input-Output in Java

---
+++ Java/InOut.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class InOut{
+    public static void main(String[] args) {
+        //output
+        /*System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        //variables 
+        
+        int a=25;
+        int b=10;
+        int sum=a+b;
+        System.out.println(sum);
+        int diff=a-b;
+        System.out.println(diff);
+        int mul=a*b;
+        System.out.println(mul);
+        double div=a/b;
+        System.out.println(div);
+
+        //input
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        int age=sc.nextInt();
+        float fees=sc.nextFloat();
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(fees);
+        */
+
+       //take 2 variables 'a' and 'b' and print their sum
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int sum=a+b;
+       System.out.println("Sum=" + sum);
+
+    }
+}
